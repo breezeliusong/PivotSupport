12a14fb3709679ca4ad03bfe8b531b45796841a2
v-liuson <v-liuson@MSDNUWPSERVER1>
2017-01-10 19:37:13 +0800

    Add project files.

---
+++ PivotSupport/DataModel/PriceVm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace PivotSupport.DataModel
+{
+     public class PriceVm: INotifyPropertyChanged
+    {
+        public string Code { get; set; }
+        public string ArticleName { get; set; }
+
+
+        private Brush _textcolor = new SolidColorBrush(Colors.Black);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void RaisedPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+
+        public Brush textcolor
+        {
+            get { return _textcolor; }
+            set
+            {
+                _textcolor = value;
+                RaisedPropertyChanged("textcolor");
+            }
+        }
+
+    }
+}
---
+++ PivotSupport/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using GalaSoft.MvvmLight;
+using PivotSupport.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PivotSupport
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public ObservableCollection<PriceVm> recordingList { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisedPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+
+        private object _PivotSelectedItem;
+        public object PivotSelectedItem
+        {
+            get { return _PivotSelectedItem; }
+            set
+            {
+                if (_PivotSelectedItem != null)
+                {
+                    var temp = _PivotSelectedItem as PriceVm;
+                    if (temp != null)
+                    {
+                        temp.textcolor = new SolidColorBrush(Colors.Black);
+                    }
+                }
+                _PivotSelectedItem = value;
+                var temp2 = _PivotSelectedItem as PriceVm;
+                if (temp2 != null)
+                {
+                    temp2.textcolor = new SolidColorBrush(Colors.Red);
+                }
+                _PivotSelectedItem = temp2 as PriceVm;
+
+                RaisedPropertyChanged("PivotSelectedItem");
+            }
+        }
+        public MainPage()
+        {
+            this.InitializeComponent();
+            PriceVm item1 = new PriceVm() { Code = "hello", ArticleName = "world"};
+            PriceVm item2 = new PriceVm() { Code = "hi", ArticleName = "boy"};
+            recordingList = new ObservableCollection<PriceVm>();
+            recordingList.Add(item1);
+            recordingList.Add(item2);
+        }
+    }
+}
